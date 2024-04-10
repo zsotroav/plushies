b763bcce4bfeb0bd2cf30ef96ff176346e381a8a
zsotroav <zsotroav@gmail.com>
2024-04-10 10:21:21 +0200

    Terminal size warning

--- src/game.cpp
+++ src/game.cpp
@@ -26,6 +26,22 @@ void gameInit() {
     // Econio raw mode to have keyboard input directly
     econio_rawmode();
 
+    econio_textcolor(COL_RED);
+    wcout << "* ATTENTION: Please make sure your terminal screen buffer size is"
+             " at least 120 characters x 30 lines then press any key." << endl;
+
+    econio_gotoxy(55, 29);
+    wcout << "You are good if you can read both of these lines at the same time";
+
+    econio_textcolor(COL_WHITE);
+    econio_gotoxy(46, 14);
+    wcout << "Plush Monsters' Fight Club" << endl;
+
+    while (!econio_kbhit()) econio_sleep(0.2);
+
+    econio_clrscr();
+
+
     GameMode gm;
     int cnt, opp;
     menu(gm, cnt, opp);
