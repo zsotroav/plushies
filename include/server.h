eeb33e4352a5b8b354441d72c7b2ef237b95709d
zsotroav <zsotroav@gmail.com>
2024-05-17 14:22:41 +0200

    server cleanup

--- include/server.h
+++ include/server.h
@@ -21,6 +21,13 @@ namespace plushies {
         class Connection;
     }
 
+    /**
+     * Read in a line from a CSV file
+     * @param ifstream Stream to read from
+     * @return vector of strings
+     */
+    std::vector<string> readCSV(std::ifstream& ifstream);
+
     class Server {
         EnemyMode enemyMode;
         GameMode game_mode_;
@@ -34,39 +41,62 @@ namespace plushies {
         void registerOpponents(int cnt);
     public:
 
-        [[nodiscard]] Player* getPlayer(const int i) const
-        { return players[i]; }
+        std::vector<Brand> brands; //!< Available Brands in the current game
+        std::vector<Action> actions; //!< Available actions in the current game
 
-        [[nodiscard]] EnemyMode getLanMode() const { return enemyMode; }
-        [[nodiscard]] bool againstLAN() const
+        /**
+         * Get a player from the server
+         * @param i ID of player
+         * @return Pointer to the player
+         */
+        Player* getPlayer(const int i) const { return players[i]; }
+
+        EnemyMode getLanMode() const { return enemyMode; } //!< Get enemy mode
+        bool againstLAN() const //!< Check if playing against LAN player
         { return enemyMode == LAN_CLIENT || enemyMode == LAN_SERVER; }
 
-        [[nodiscard]] GameMode getGameMode() const { return game_mode_; }
+        GameMode getGameMode() const { return game_mode_; } //!< Get the game mode
 
+        /**
+         * Register a communicator for LAN Play
+         * @param c Communicator to register
+         * @throws std::invalid_argument If called in non LAN context
+         */
         void registerComm(nyetwork::Communicator* c);
 
         /**
-         * @brief Available Brands in the current game
-         */
-        std::vector<Brand> brands;
-
-        /**
-         * @brief Available actions in the current game
-         */
-        std::vector<Action> actions;
-
-        /**
-         * @brief Generate a random plush with optional strength restrictions 
+         * Generate a random plush with optional strength restrictions
          * @param bst Minimum Base Stat Total (negative for maximum)
          * @param uvmin minimum values for UVs
          * @param uvmax maximum values for UVs
          */
         Plush createRandomPlush(int bst = 0, int uvmin = 0, int uvmax = 63);
 
+        /**
+         * Start the main server loop
+         * @return The game end result
+         *  - 1 : Won
+         *  - 0 : Lost
+         *  - -1: Catastrophic internal error (couldn't recover)
+         */
         int serverLoop();
 
+        /**
+         * Register a player to the server
+         * @param p Player pointer to register (deletes: server)
+         * @param num ID of player
+         */
         void RegisterPlayer(Player* p, const int num = 0) { players[num] = p; }
 
+        /**
+         * Initialize the server and all data
+         * @param em Enemy Mode to start with
+         * @param gm Game Mode
+         * @param cnt Number of plushes
+         * @param brandFile Brand csv location
+         * @param actionFile Actions csv location
+         * @param actionLearnFile Action Learn csv location
+         */
         Server(EnemyMode em, GameMode gm, int cnt,
                const string& brandFile = "data/brands.csv",
                const string& actionFile = "data/actions.csv",
